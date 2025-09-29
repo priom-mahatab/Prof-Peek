801a93baa28669cfb468fb3be29809e217ae9436
Zohaer Al Mahatab <133798007+priom-mahatab@users.noreply.github.com>
2025-09-28 21:43:29 -0500

    Setup manifest and content files

---
+++ content.js
@@ -0,0 +1,53 @@
+console.log("RMP Extension loaded, St. Edward's University");
+
+// create function for formatting name
+function formatName(rawName) {
+    const parts = rawName.split(",");
+    if (parts.length === 2) {
+        const last = parts[0].trim();
+        const first = parts[1].trim();
+
+        return `${first} ${last}`;
+    }
+
+    return rawName.trim();
+}
+
+// function for injecting rating. add a class and then add text and styling
+function injectRating(link, ratingData) {
+    const badge = document.createElement("span");
+    link.classList.add("rmp-badge");
+    badge.textContent = `Rating: ${ratingData.rating} | Difficulty: ${ratingData.difficulty}`;
+    badge.style.marginLeft = "8px";
+    badge.style.padding = "2px 6px";
+    badge.style.borderRadius = "6px";
+    badge.style.fontSize = "12px";
+    badge.style.color = "#333";
+}
+
+// function for extracting info from course catalogue
+function extractAndDisplayInstructors() {
+    const instructorCells = document.querySelectorAll("td[data-property='instructor'] a.email");
+    instructorCells.forEach(link => {
+        const rawName = link.textContent.trim();
+        const formattedName = formatName(rawName);
+        console.log("Instructor", formattedName);
+        }
+    )
+
+    const fakeData = {
+        rating: "4.2",
+        difficulty: "3.1"
+    }
+
+    if (!link.nextSibling || !link.nextSibling.classList?.contains("rmp-badge")) {
+        injectRating(link, fakeData);
+    }
+}
+
+extractAndDisplayInstructors();
+
+const observer = new MutationObserver(() => extractAndDisplayInstructors());
+observer.observe(document.body, {childList: true, subtree: true});
+
+
