5a1e3b1e4a69b314dacd464de274a37aefa081f4
Zohaer Al Mahatab <133798007+priom-mahatab@users.noreply.github.com>
2025-09-29 00:27:33 -0500

    Modify content.js

--- content.js
+++ content.js
@@ -7,22 +7,29 @@ function formatName(rawName) {
         const last = parts[0].trim();
         const first = parts[1].trim();
 
+        if (first.split(" ").length === 2) {
+            const shortFirst = first.split(" ")[0].trim()
+            return `${shortFirst} ${last}`;
+        }
         return `${first} ${last}`;
     }
 
     return rawName.trim();
 }
 
-// function for injecting rating. add a class and then add text and styling
+// function for injecting rating add a class and then add text and styling
 function injectRating(link, ratingData) {
     const badge = document.createElement("span");
-    link.classList.add("rmp-badge");
     badge.textContent = `Rating: ${ratingData.rating} | Difficulty: ${ratingData.difficulty}`;
+    badge.classList.add("rmp-badge");
     badge.style.marginLeft = "8px";
     badge.style.padding = "2px 6px";
+    badge.style.background = "#f0f0f0";
     badge.style.borderRadius = "6px";
     badge.style.fontSize = "12px";
     badge.style.color = "#333";
+
+    link.insertAdjacentElement("afterend", badge);
 }
 
 // function for extracting info from course catalogue
@@ -31,22 +38,24 @@ function extractAndDisplayInstructors() {
     instructorCells.forEach(link => {
         const rawName = link.textContent.trim();
         const formattedName = formatName(rawName);
+        const [firstName, lastName] = formattedName.split(" ");
         console.log("Instructor", formattedName);
+
+        const fakeData = {
+            rating: "4.2",
+            difficulty: "3.6"
         }
-    )
 
-    const fakeData = {
-        rating: "4.2",
-        difficulty: "3.1"
+        if (!link.nextSibling || !link.nextSibling.classList?.contains("rmp-badge")) {
+            injectRating(link, fakeData);
+        }
     }
-
-    if (!link.nextSibling || !link.nextSibling.classList?.contains("rmp-badge")) {
-        injectRating(link, fakeData);
-    }
+  );
 }
 
 extractAndDisplayInstructors();
 
+// observer for looking for updates in the DOM
 const observer = new MutationObserver(() => extractAndDisplayInstructors());
 observer.observe(document.body, {childList: true, subtree: true});
 
