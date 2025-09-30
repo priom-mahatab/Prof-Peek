9cbc300e37f16650f56b402e31edd3e8fb0d748f
Zohaer Al Mahatab <133798007+priom-mahatab@users.noreply.github.com>
2025-09-30 17:55:28 -0500

    Implement caching and update logos

--- src/background.ts
+++ src/background.ts
@@ -1,23 +1,52 @@
 import { RateMyProfessor } from "rate-my-professor-api-ts"
 
+const CACHE_TTL = 24 * 60 * 60 * 1000;
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "getRating") {
-        (async () => {
+        const professor = message.professor;
+
+        chrome.storage.local.get([professor], async (result) => {
+            const cached = result[professor];
+
+            if (cached && Date.now() - cached.timestamp < CACHE_TTL) {
+                console.log(`Cache hit for ${professor}`);
+                sendResponse({ ratingData: cached.value });
+                return;
+            }
+
             try {
-                const rmp_instance = new RateMyProfessor("St. Edward's University", message.professor);
+                console.log(`Fetching fresh data`);
+                const rmp_instance = new RateMyProfessor("St. Edward's University", professor);
                 const data = await rmp_instance.get_professor_info();
 
-                sendResponse({
-                    ratingData: {
-                        rating: data?.avgRating?.toString() ?? "N/A",
-                        difficulty: data?.avgDifficulty?.toString() ?? "N/A"
+                const ratingData = {
+                    rating: data?.avgRating?.toString() ?? "N/A",
+                    difficulty: data?.avgDifficulty?.toString() ?? "N/A",
+                };
+
+                // save to cache
+                chrome.storage.local.set({
+                    [professor]: {
+                        value: ratingData,
+                        timestamp: Date.now(),
                     }
                 });
+
+                sendResponse({ ratingData });
+
             } catch (err) {
-                console.error("RMP fetch failed", err);
-                sendResponse({ ratingData: {rating: "N/A", difficulty: "N/A" }})
+                console.error("RMP fetch failed:", err);
+                sendResponse({
+                    ratingData: {
+                        rating: "N/A",
+                        difficulty: "N/A"
+                    },
+                })
             }
-        })();
+        
+
+        })
 
         return true;
     }
--- src/rmp-api.ts
+++ src/rmp-api.ts
@@ -1,6 +1,6 @@
 import { RateMyProfessor } from "rate-my-professor-api-ts";
 (async function main() {
-    const rmp_instance = new RateMyProfessor("St. Edward's University", "Megan Avery");
+    const rmp_instance = new RateMyProfessor("St. Edward's University", "Monica Cicciarelli");
 
     console.log(await rmp_instance.get_professor_info());
 })();
